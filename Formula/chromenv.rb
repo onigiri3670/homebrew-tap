34bf96acc988e4dc55d5d660d7c86a1ba4237b86
ItogaTakuma <takuma.itoga@rooter.co.jp>
2021-02-28 16:09:37 +0900

    Revert "installメソッドの呼び出しのデバッグ"
    This reverts commit 41a306b022b307c06e00f41fd1a2a1fda3a5ce98.

--- Formula/chromenv.rb
+++ Formula/chromenv.rb
@@ -11,14 +11,29 @@ class Chromenv < Formula
   # depends_on "cmake" => :build
 
   def install
-    puts 'installメソッドだよ'
-    # p libexec
-    # p HOMEBREW_PREFIX
-    # inreplace "bin/chromenv" do |s|
-    #   p s
-    #   # s.gsub! '"${BASH_SOURCE%/*}"/../libexec', libexec
-    #   s.sub! "CHROMENV_HOME=$(pwd)", "CHROMENV_HOME=#{HOMEBREW_PREFIX}"
-    # end
-    # prefix.install ["bin", "libexec"]
+    p libexec
+    p HOMEBREW_PREFIX
+    inreplace "bin/chromenv" do |s|
+      p s
+      # s.gsub! '"${BASH_SOURCE%/*}"/../libexec', libexec
+      s.sub! "CHROMENV_HOME=$(pwd)", "CHROMENV_HOME=#{HOMEBREW_PREFIX}"
+    end
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    prefix.install ["bin", "libexec"]
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test chromenv`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
